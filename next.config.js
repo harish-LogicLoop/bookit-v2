8a44a7fa193370e6df67fb48512d8c92be5d4cdd
Harish Patel <harish@logicloop.io>
2024-06-18 11:13:19 +0100

    changes to how db connectivity is established

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   env: {
     API_URL: "http://localhost:3000",
-    DB_LOCAL_URI: "mongodb://127.0.0.1:27017/bookit-v2",
+    DB_LOCAL_URI: "mongodb://root:Unicorn3214@127.0.0.1:27017/bookit-v2",
     DB_URI: "",
 
     CLOUDINARY_CLOUD_NAME: "",
